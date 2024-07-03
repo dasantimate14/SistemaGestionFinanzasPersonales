ea706ed58c09dfa81a601f42c9d06e50f3e5830c
Diego Santimateo <diego.santimateo@utp.ac.pa>
2024-07-03 00:35:28 -0500

    Adición de los atributos de la superclase FinanceItem.

--- sistemagestionfinanzas/FinanceItem.java
+++ sistemagestionfinanzas/FinanceItem.java
@@ -1,4 +1,19 @@
 package sistemagestionfinanzas;
 
+import java.time.LocalDate;
+
 public class FinanceItem {
+    protected String nombre;
+    protected String descripcion;
+    protected float monto;
+    protected String tipo;
+    protected float porcentajeGanancia;
+    protected String id;
+    protected float tasaInteres;
+    protected float interes;
+    protected LocalDate fechaInicio;
+    protected float montoTotal;
+    protected float promedio;
+
+
 }
