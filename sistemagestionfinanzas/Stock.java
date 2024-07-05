44ca6fcfecff1a5071ed0ab4ea2909f7659122c0
Diego Santimateo <diego.santimateo@utp.ac.pa>
2024-07-04 23:49:36 -0500

    Codificacion de metodo para calcular la cantidad de dividendo acumulados hasta la actualidad y correccion del constructor

--- sistemagestionfinanzas/Stock.java
+++ sistemagestionfinanzas/Stock.java
@@ -1,22 +1,26 @@
 package sistemagestionfinanzas;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Stock extends FinanceItem{
     //Atributos de la clase
-    String nombreEmpresa;
-    String simbolo;
-    int cantidad;
-    float precioCompra;
-    String sector;
-    float precioActual;
-    float dividendoPorAccion;
-    float dividendoAcumulado;
-    static int cantidadInstancias = 0;
-    static Stock[] instanciasStocks = new Stock[cantidadInstancias + 1];
+    private String nombreEmpresa;
+    private String simbolo;
+    private int cantidad;
+    private float precioCompra;
+    private String sector;
+    private float precioActual;
+    private float dividendoPorAccion;
+    private float dividendoAcumulado;
+    private int frecuenciaDividendos;
+    private static int cantidadInstancias = 0;
+    private static List<Stock> instanciasStocks = new ArrayList<>();
 
     public Stock(String nombre, String descripcion, float tasaInteres, LocalDate fechaInicio,
-                 String nombreEmpresa, String simbolo, int cantidad, float precioCompra, String sector, float dividendoPorAccion){
+                 String nombreEmpresa, String simbolo, int cantidad, float precioCompra, String sector, float dividendoPorAccion, int frecuenciaDividendos){
         super(nombre, descripcion, (cantidad*precioCompra), "Activo", tasaInteres, fechaInicio);
         this.nombreEmpresa = nombreEmpresa;
         this.simbolo = simbolo;
@@ -24,20 +28,17 @@ public class Stock extends FinanceItem{
         this.precioCompra = precioCompra;
         this.sector = sector;
         this.dividendoPorAccion = dividendoPorAccion;
+        this.frecuenciaDividendos = frecuenciaDividendos;
 
         //Se guarda la instancia dentro de un arreglo que pertenece a la clase misma y no a la instancia
-        instanciasStocks[cantidadInstancias] = this;
+        instanciasStocks.add(this);
         cantidadInstancias++;
     }
 
     // Constructor con dividendoPorAccion por defecto
     public Stock(String nombre, String descripcion, float tasaInteres, LocalDate fechaInicio,
                  String nombreEmpresa, String simbolo, int cantidad, float precioCompra, String sector) {
-        this(nombre, descripcion, tasaInteres, fechaInicio, nombreEmpresa, simbolo, cantidad, precioCompra, sector, 0.0f);
-
-        //Se guarda la instancia dentro de un arreglo que pertenece a la clase misma y no a la instancia
-        instanciasStocks[cantidadInstancias] = this;
-        cantidadInstancias++;
+        this(nombre, descripcion, tasaInteres, fechaInicio, nombreEmpresa, simbolo, cantidad, precioCompra, sector, 0.0f, 0);
     }
 
     //Metodos get y set de la clase
@@ -47,7 +48,10 @@ public class Stock extends FinanceItem{
     public float getPrecioCompra() {return precioCompra;}
     public String getSector() {return sector;}
     public float getDividendoPorAccion() {return dividendoPorAccion;}
+    public float getDividendoAcumulado(){return dividendoAcumulado;}
+    public static int getCantidadInstancias() {return cantidadInstancias;}
     public float getPrecioActual(){return precioActual;}
+    public int getFrecuenciaDividendos(){return frecuenciaDividendos;}
 
     public void setNombreEmpresa(String nombreEmpresa){this.nombreEmpresa = nombreEmpresa;}
     public void setSimbolo(String simbolo){this.simbolo = simbolo;}
@@ -55,6 +59,7 @@ public class Stock extends FinanceItem{
     public void setPrecioCompra(float precioCompra){this.precioCompra = precioCompra;}
     public void setSector(String sector){this.sector = sector;}
     public void setDividendoPorAccion(float dividendoPorAccion){this.dividendoPorAccion = dividendoPorAccion;}
+    public void setDividendoAcumulado(float dividendoAcumulado){this.dividendoAcumulado = dividendoAcumulado;}
     public void setPrecioActual(float precioActual){this.precioActual = precioActual;}
 
     @Override
@@ -71,6 +76,7 @@ public class Stock extends FinanceItem{
         sb.append("Cantidad: ").append(cantidad).append("\n");
         sb.append("Precio de Compra: ").append(precioCompra).append("\n");
         sb.append("Dividendo por Acción: ").append(dividendoPorAccion).append("\n");
+        sb.append("Frecuencia de Pago de Dividendos: ").append(frecuenciaDividendos).append("\n");
         sb.append("Precio Actual: ").append(precioActual).append("\n");
         sb.append("Dividendo Acumulado: ").append(dividendoAcumulado).append("\n");
         sb.append("Sector: ").append(sector).append("\n");
@@ -130,7 +136,27 @@ public class Stock extends FinanceItem{
     }
 
     public float calcularDividendoAcumulado(){
+        LocalDate fechaHoy = LocalDate.now();
+        LocalDate fechaCompra = getFechaInicio();
+        //Solo se calcula el dividiendo acumulado si es diferente de cero
+        if(getDividendoPorAccion() != 0){
+            while(!fechaCompra.isAfter(fechaHoy)){
+                //Se debe acumular el dividendo si es el primero del mes
+                if(fechaCompra.getDayOfMonth() == 1 && fechaCompra.equals(getFechaInicio())){
+                    dividendoAcumulado += (dividendoPorAccion * cantidad);
+                } else if (fechaCompra.isBefore(fechaHoy)){
+                    dividendoAcumulado += (dividendoPorAccion * cantidad);
+                }
+                //Avanza al proximo mes
+                fechaCompra = fechaCompra.plusMonths(frecuenciaDividendos);
+            }
+
+        }
         return dividendoAcumulado;
+    }
+
+    public float calculadDividendoFuturo(int meses){
+        return 0;
     }
 
     public float obtenerPrecioActual(){
