d90a5492451e40414f8da6c627427f65059f6b7a
Luz <luzmadlpotter@gmail.com>
2024-07-06 22:51:09 -0500

    Introducción de métodos, gets y sets de la clase TarjetaCredito

--- sistemagestionfinanzas/TarjetaCredito.java
+++ sistemagestionfinanzas/TarjetaCredito.java
@@ -1,4 +1,72 @@
 package sistemagestionfinanzas;
 
 public class TarjetaCredito {
+    private String tipoTarjeta;
+    private float limiteCredito;
+    private float saldoActual;
+    private float tasaInteres;
+    private String numero;
+    private float creditoUsado;
+
+    // Constructor
+    public TarjetaCredito(String tipoTarjeta, float limiteCredito, float saldoActual, float tasaInteres, String numero) {
+        this.tipoTarjeta = tipoTarjeta;
+        this.limiteCredito = limiteCredito;
+        this.saldoActual = saldoActual;
+        this.tasaInteres = tasaInteres;
+        this.numero = numero;
+        this.creditoUsado = calcularCreditoUsado();
+    }
+
+    // Métodos para calcular el crédito usado
+    private float calcularCreditoUsado() {
+        return limiteCredito - saldoActual;
+    }
+
+    // Getters y Setters
+    public String getTipoTarjeta() {
+        return tipoTarjeta;
+    }
+
+    public void setTipoTarjeta(String tipoTarjeta) {
+        this.tipoTarjeta = tipoTarjeta;
+    }
+
+    public float getLimiteCredito() {
+        return limiteCredito;
+    }
+
+    public void setLimiteCredito(float limiteCredito) {
+        this.limiteCredito = limiteCredito;
+        this.creditoUsado = calcularCreditoUsado();
+    }
+
+    public float getSaldoActual() {
+        return saldoActual;
+    }
+
+    public void setSaldoActual(float saldoActual) {
+        this.saldoActual = saldoActual;
+        this.creditoUsado = calcularCreditoUsado();
+    }
+
+    public float getTasaInteres() {
+        return tasaInteres;
+    }
+
+    public void setTasaInteres(float tasaInteres) {
+        this.tasaInteres = tasaInteres;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public float getCreditoUsado() {
+        return creditoUsado;
+    }
 }
