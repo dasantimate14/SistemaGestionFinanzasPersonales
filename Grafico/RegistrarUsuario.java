c62f1b7b5f45957accce29c390b8d838bfcedd08
daniella <daniella.deleon@utp.ac.pa>
2024-07-11 15:13:14 -0500

    Termine el GUI de Registrar

--- Grafico/RegistrarUsuario.java
+++ Grafico/RegistrarUsuario.java
@@ -4,6 +4,13 @@ import javax.swing.*;
 
 public class RegistrarUsuario {
     private JPanel RegistrarPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JPasswordField passwordField1;
+    private JButton enviarButton;
+    private JButton yaTengoUnaCuentaButton;
 
     public void setVisible(boolean b) {
     }
