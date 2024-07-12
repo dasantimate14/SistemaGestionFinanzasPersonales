f678f55d11da611fd3e4ca1297357d3db6193425
daniella <daniella.deleon@utp.ac.pa>
2024-07-12 00:26:15 -0500

    creación plantilla Tarjetas de credito

---
+++ Grafico/Stocks.java
@@ -0,0 +1,16 @@
+package Grafico;
+
+import javax.swing.*;
+
+public class Stocks {
+    private JPanel StocksPanel;
+    private JButton menuPrincipalButton;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+}
