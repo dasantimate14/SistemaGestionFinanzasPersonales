4d01b82b83d297545c9e908ce08044d1d061c2ec
daniella <daniella.deleon@utp.ac.pa>
2024-07-12 00:38:03 -0500

    creación plantilla Gastos y terminacion de prestamos

---
+++ Grafico/Prestamos.java
@@ -0,0 +1,15 @@
+package Grafico;
+
+import javax.swing.*;
+
+public class Prestamos {
+    private JButton menuPrincipalButton;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+}
