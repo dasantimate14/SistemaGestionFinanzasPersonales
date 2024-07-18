f14f5ed256441dff239612ca36b6d12f62e094c3
Luz <luzmadlpotter@gmail.com>
2024-07-18 16:16:35 -0500

    Se introducen los elementos de la interfaz de la clase Prestamos.form

--- Grafico/Prestamos.java
+++ Grafico/Prestamos.java
@@ -3,13 +3,25 @@ package Grafico;
 import javax.swing.*;
 
 public class Prestamos {
-    private JButton menuPrincipalButton;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
+    private JButton páginaPrincipalButton;
+    private JButton cuentasBancariasButton;
+    private JButton plazosFijosButton;
+    private JButton ingresosYGastosButton;
+    private JButton stocksButton;
+    private JButton tarjetasDeCréditoButton;
+    private JButton préstamosButton;
+    private JButton proyeccionesButton;
     private JButton button9;
+    private JButton agregarPréstamoButton;
+    private JButton mostrarInterésPendienteButton;
+    private JButton mostrarInterésAcumuladoButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JList list1;
+    private JComboBox comboBox1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JComboBox comboBox2;
+    private JTextField textField5;
+    private JPanel FechaDeVencimientoPanel;
 }
