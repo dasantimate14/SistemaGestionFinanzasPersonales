41f0b62539afb0adc5b04fe3168e171fd2fb3fad
Diego Santimateo <diego.santimateo@utp.ac.pa>
2024-07-21 02:17:21 -0500

    Adición de datePicker a interfaz de Stocks

--- Grafico/Stocks.java
+++ Grafico/Stocks.java
@@ -1,18 +1,27 @@
 package Grafico;
 
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
 import sistemagestionfinanzas.Stock;
 import sistemagestionfinanzas.Usuario;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Properties;
 
 public class Stocks extends JFrame {
     private JPanel StocksPanel;
     private JButton menuPrincipalButton;
+    private JPanel datePanelContainer;
     private JButton cuentaBancariaButton;
     private JButton prestamosButton;
     private JButton tarjetasDeCreditoButton;
@@ -27,13 +36,13 @@ public class Stocks extends JFrame {
     private JButton agregarNuevoStockButton;
     private JTable tablaStocks;
     private JTextField tdDescripcion;
-    private JScrollBar scrollBar1;
     private JTextField tfSimbolo;
     private JTextField tfPrecioCompra;
     private JTextField tfDividendoAccio;
     private JTextField tfFrecuenciaDividendos;
     private JTextField tfPrecioSctual;
     private JComboBox<String> cbFrecuenciaIng;
+    private JDatePickerImpl datePicker;
 
 
     public Stocks() {
@@ -46,6 +55,18 @@ public class Stocks extends JFrame {
         setContentPane(StocksPanel);
 
         configureNavigationButtons();
+
+        // Implementación del JDatePicker dentro del GUI
+        UtilDateModel model = new UtilDateModel();
+        Properties p = new Properties();
+        p.put("text.today", "Hoy");
+        p.put("text.month", "Mes");
+        p.put("text.year", "Año");
+        JDatePanelImpl datePanelImpl = new JDatePanelImpl(model, p);
+        datePicker = new JDatePickerImpl(datePanelImpl, new DateLabelFormatter());
+
+        datePanelContainer.setLayout(new BorderLayout());
+        datePanelContainer.add(datePicker, BorderLayout.CENTER);
 
         agregarNuevoStockButton.addActionListener(new ActionListener() {
             @Override
@@ -60,13 +81,6 @@ public class Stocks extends JFrame {
             }
         });
 
-        // Añadir un AdjustmentListener al JScrollBar
-        scrollBar1.addAdjustmentListener(new AdjustmentListener() {
-            @Override
-            public void adjustmentValueChanged(AdjustmentEvent e) {
-                System.out.println("El valor de la barra de desplazamiento es: " + scrollBar1.getValue());
-            }
-        });
     }
 
     private void configureNavigationButtons() {
@@ -227,4 +241,24 @@ public class Stocks extends JFrame {
             }
         });
     }
+    // Formatter para que la librería se extienda
+    public class DateLabelFormatter extends JFormattedTextField.AbstractFormatter {
+        private String datePattern = "dd/MM/yyyy";
+        private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+
+        @Override
+        public Object stringToValue(String text) throws ParseException {
+            return dateFormatter.parseObject(text);
+        }
+        @Override
+        public String valueToString(Object value) throws ParseException {
+            if (value != null) {
+                Calendar cal = (Calendar) value;
+                return dateFormatter.format(cal.getTime());
+            }
+            return"";
+        }
+
+    }
 }
+
